290d9dd06bd3820353468f5e29cd6f11c4c53917
REKVID <rekvid2020@mail.ru>
2025-06-20 05:03:10 +0500

    BD update

--- src/main/java/com/ordersystem/db/init_db.sql
+++ src/main/java/com/ordersystem/db/init_db.sql
@@ -20,7 +20,7 @@ CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,          
     name VARCHAR(50) NOT NULL,                 
     price DECIMAL(10, 2) NOT NULL,              
-    description TEXT,                           
+    description TEXT NOT NULL,                           
     is_delivery_available BOOLEAN NOT NULL      
 );
 
